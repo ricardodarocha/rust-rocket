da74a81f513f8f5302b9f0f53ef179352fa1eb00
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-02-26 21:51:31 -0300

    Refactory

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,65 @@ struct User {
 
 //database todo! Move to database.rs
 
-use sqlx::SqliteConnection;
+use sqlx::{Database, Sqlite, SqliteConnection};
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![
+        get_one,
         get_all,
-        get_by_id])
+        ])
 }
 
 //routes todo! Move to routes.rs
 #[macro_use]
 extern crate dotenv_codegen;
 
-#[get("/all")]
-async fn get_all() -> Json<Vec<User>> { 
+async fn get_sql<T>(sql: &str) -> Json<Vec<T>> 
+where
+   T: for<'r> sqlx::FromRow<'r, <Sqlite as Database>::Row>,
+   T: Send,
+   T: Unpin,
+{ 
     let mut connection: SqliteConnection = SqliteConnection::connect(dotenv!("DATABASE_URL"))
         .await
         .unwrap();
 
-    let result = sqlx::query_as!(User, "SELECT * FROM users")
-    .fetch_all(&mut connection)
-    .await
-    .unwrap();
-
-    return Json(result);
-}
-
-
-#[get("/one/<id>")]
-async fn get_by_id(id: u32) -> Json<Vec<User>> {  
-    let mut connection: SqliteConnection = SqliteConnection::connect(dotenv!("DATABASE_URL"))
-        .await
-        .unwrap();
-
-    let result = sqlx::query_as!(User, "SELECT * FROM users where id = ?", id)
+ let result = sqlx::query_as::<_, T>(sql)
         .fetch_all(&mut connection)
         .await
         .unwrap();
 
     return Json(result);
+}
+
+async fn get_by_id<T>(sql: &str, id: u32) -> Json<T> 
+where
+   T: for<'r> sqlx::FromRow<'r, <Sqlite as Database>::Row>,
+   T: Send,
+   T: Unpin,
+{ 
+    let mut connection: SqliteConnection = SqliteConnection::connect(dotenv!("DATABASE_URL"))
+        .await
+        .unwrap();
+
+ let result = sqlx::query_as::<_, T>(sql)
+        .bind(id)
+        .fetch_one(&mut connection)
+        .await
+        .unwrap();
+
+    return Json(result);
+}
+
+
+#[get("/all")]
+async fn get_all() -> Json<Vec<User>> { 
+    get_sql::<User>("select * from users").await
+}
+
+
+#[get("/one/<id>")]
+async fn get_one(id: u32) -> Json<User> {  
+    get_by_id::<User>("SELECT * FROM users where id = ?", id).await
 }
